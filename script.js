d0a8433330fa176e50e6372f8f3b20a0f8f3d365
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-16 03:15:46 -0400

    created characterChoice function

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment code here
 var generatePassword = function () {
-  // Enter character length
+  // Choose password character length
   var passLength = passwordLength();
 
-  // switch to generate password based on choices from prompts
+  // Choose character types in password
   var charChoice = characterChoice();
 
-  // return password based on selections
-  for (i = 1; 1 <= passLength; i++) {
+
+  // generate password based on selections
+  for (var i = 1; 1 <= passLength; i++) {
 
   }
   
@@ -20,12 +21,14 @@ var generatePassword = function () {
 // prompt for password character length
 var passwordLength = function() {
   var characterLength = window.prompt("How many characters should your password be? Please enter a number between 8 and 128.");
-  // make sure charracterLength is a number between 8 and 128
+  
+  // make sure user actually typed something into the prompt
   while (characterLength === "" || characterLength === null) {
       window.alert("Please enter a number between 8 and 128.");
       return passwordLength();
   }
-  // make user's response into integer
+  
+  // convert user's response into integer
   characterLength = parseInt(characterLength);
 
   // make sure user's response is a number between 8 and 128
@@ -41,6 +44,16 @@ var passwordLength = function() {
 
 // prompts for character types: lowercase, uppercase, numeric, and/or special characters
 var characterChoice = function() {
+
+  var charChoice = "";
+
+  // define different character types
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "0123456789";
+  var symbol = "!?@#$%^&*()_";
+
+  // character selection prompts
   var lowercaseConfirm = window.confirm("Should your password contain lowercase characters?");
 
   var uppercaseConfirm = window.confirm("Should your password contain uppercase characters?");
@@ -53,7 +66,26 @@ var characterChoice = function() {
   if (lowercaseConfirm === false && uppercaseConfirm === false && numericConfirm === false && specialConfirm === false) {
     window.alert("Please select at least one character type.");
     // start character select function again
+    characterChoice();
   }
+
+  if (lowercaseConfirm) {
+    charChoice = charChoice + lower;
+  }
+
+  if (uppercaseConfirm) {
+    charChoice = charChoice + upper;
+  }
+
+  if (numericConfirm) {
+    charChoice = charChoice + number;
+  }
+
+  if (specialConfirm) {
+    charChoice = charChoice + symbol;
+  }
+
+  return charChoice;
 };
 
 
